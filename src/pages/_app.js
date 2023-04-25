8907a4fdb564f00f06c199afb0899d4325e10712
Sajana Bupathi Herath <65496567+Sajana2103@users.noreply.github.com>
2023-04-25 11:20:08 +0530

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
+import { Figtree } from 'next/font/google'
 
+const figtree = Figtree({ subsets: ['latin'] })
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+  <main className={figtree.className}>
+
+    <Component {...pageProps} />
+  </main>
+  )
 }
